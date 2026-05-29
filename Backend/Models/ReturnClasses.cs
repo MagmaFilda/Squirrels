7e7963132e5e0488afcd9a93d2d1c61c54986e7f
MagmaFilda <kamencak.filip@seznam.cz>
2026-05-29 11:27:39 +0200

    Fixed returnInv

--- Backend/Models/ReturnClasses.cs
+++ Backend/Models/ReturnClasses.cs
@@ -2,24 +2,12 @@
 {
     public class ReturnInventory
     {
-        public string Name { get; private set; }
-        public string Description { get; private set; }
-        public Rarity Rarity { get; private set; }
-        public int Strength { get; private set; }
-        public int Speed { get; private set; }
-        public int Durability { get; private set; }
-        public int Cost { get; private set; }
+        public Squirrel ReturningSquirrel {  get; private set; }
         public int Count { get; private set; }
 
         public ReturnInventory(Squirrel squirrel, int count)
         {
-            Name = squirrel.Name;
-            Description = squirrel.Description;
-            Rarity = squirrel.Rarity;
-            Strength = squirrel.Strength;
-            Speed = squirrel.Speed;
-            Durability = squirrel.Durability;
-            Cost = squirrel.Cost;
+            ReturningSquirrel = squirrel;
             Count = count;
         }
     }
@@ -27,7 +15,7 @@
     public class RegisterRequest
     {
         public string Username { get; set; }
-        public string?Password { get; set; }
+        public string Password { get; set; }
         public string Email { get; set; }
     }
     public class LoginRequest
